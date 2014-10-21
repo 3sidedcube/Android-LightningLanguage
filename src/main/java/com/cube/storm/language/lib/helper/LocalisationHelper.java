0cd198669b5e7d6acb6379e05d221cd0855bdaa0
Callum Taylor <callum@3sidedcube.com>
2014-10-21 14:35:44 +0100

    Added basic localisation helper class

---
+++ src/main/java/com/cube/storm/language/lib/helper/LocalisationHelper.java
@@ -0,0 +1,12 @@
+package com.cube.storm.language.lib.helper;
+
+/**
+ * Helper class for localising views in an activity/fragment/view group
+ *
+ * @author Callum Taylor
+ * @project StormLanguage
+ */
+public class LocalisationHelper
+{
+
+}
