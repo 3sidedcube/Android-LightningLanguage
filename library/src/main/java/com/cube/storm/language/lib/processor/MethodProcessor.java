f606f0aa3c891f431e408fb49aba177caa000f1e
Callum Taylor <callum@3sidedcube.com>
2015-10-19 16:46:52 +0100

    Added basic method matching and processing

--- library/src/main/java/com/cube/storm/language/lib/processor/MethodProcessor.java
+++ library/src/main/java/com/cube/storm/language/lib/processor/MethodProcessor.java
@@ -1,5 +1,10 @@
 package com.cube.storm.language.lib.processor;
 
+import android.support.annotation.NonNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Class to deal with methods on variable localisations
  *
@@ -8,13 +13,44 @@ package com.cube.storm.language.lib.processor;
  */
 public class MethodProcessor
 {
+	private static final Pattern pattern = Pattern.compile("\\{[0-9a-zA-Z_\\-\\?\\.\\(\\)\\'\\\"\\<\\>\\,\\\\\\/\\!\\@\\$\\%\\^\\&\\*]+((\\.)[0-9a-zA-Z]+)+\\}");
+	private static final Pattern methods = Pattern.compile("((\\.)[0-9a-zA-Z]+)");
+
 	/**
 	 * Processes {@param variable} based on methods attached to it
 	 *
 	 * @param variable
 	 */
-	public void process(String variable)
+	@NonNull
+	public String process(String mappedVariable, @NonNull String string)
 	{
+		Matcher matcher = pattern.matcher(string);
+		String originalString = new String(mappedVariable);
 
+		while (matcher.find())
+		{
+			String found = matcher.group();
+			Matcher methodMatcher = methods.matcher(found);
+
+			while (methodMatcher.find())
+			{
+				String method = methodMatcher.group();
+
+				if (method.equals(".upperCase"))
+				{
+					mappedVariable = upperCase(mappedVariable);
+				}
+			}
+		}
+
+		string = string.replaceAll("\\{" + originalString + "(\\.[0-9a-zA-Z]+)\\}", mappedVariable);
+		string = string.replaceAll("\\{" + originalString + "\\}", originalString);
+
+		return string;
+	}
+
+	protected String upperCase(String input)
+	{
+		return input.toUpperCase();
 	}
 }
