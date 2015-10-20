20a6e20ceddff8bc566a51bc60c5635cf4769f81
Callum Taylor <callum@3sidedcube.com>
2015-10-20 10:47:09 +0100

    Added more documentation for localise annotation

--- library/src/main/java/com/cube/storm/language/lib/annotation/Localise.java
+++ library/src/main/java/com/cube/storm/language/lib/annotation/Localise.java
@@ -4,7 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Localise annotation to use on variables when injecting localisations from variables
+ * Localise annotation to use on variables when injecting localisations from variables.
+ * <p/>
+ * You do not need to include the braces for the value in the localise annotation, only the key of the localisation
  *
  * @author Callum Taylor
  * @project LightningLanguage
