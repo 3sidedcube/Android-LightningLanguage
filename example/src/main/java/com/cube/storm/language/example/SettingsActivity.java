5b971df7c6cb9a358123c0d459f05eaaa0436bdd
Callum Taylor <callum@callumtaylor.net>
2016-04-13 16:10:36 +0100

    Formatting for PR

--- example/src/main/java/com/cube/storm/language/example/SettingsActivity.java
+++ example/src/main/java/com/cube/storm/language/example/SettingsActivity.java
@@ -19,44 +19,42 @@ import junit.framework.Assert;
  */
 public class SettingsActivity extends Activity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_settings);
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_settings);
 
 		FragmentManager fragmentManager = getFragmentManager();
 		FragmentTransaction transaction = fragmentManager.beginTransaction();
 		transaction.add(R.id.container, new SettingsActivity.SettingsFragment());
 		transaction.commit();
-    }
+	}
 
-    public static class SettingsFragment extends PreferenceFragment
-    {
-        @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
+	public static class SettingsFragment extends PreferenceFragment
+	{
+		@Override public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preferences);
 
-            PreferenceScreen preferenceScreen = getPreferenceScreen();
-            LocalisationHelper.localise(preferenceScreen);
+			PreferenceScreen preferenceScreen = getPreferenceScreen();
+			LocalisationHelper.localise(preferenceScreen);
 
-            Preference firstPreference = preferenceScreen.getPreference(0);
-            Assert.assertTrue(firstPreference instanceof PreferenceGroup);
+			Preference firstPreference = preferenceScreen.getPreference(0);
+			Assert.assertTrue(firstPreference instanceof PreferenceGroup);
 
-            PreferenceGroup preferenceGroup = (PreferenceGroup) firstPreference;
-            Assert.assertEquals("Test Category", preferenceGroup.getTitle());
+			PreferenceGroup preferenceGroup = (PreferenceGroup)firstPreference;
+			Assert.assertEquals("Test Category", preferenceGroup.getTitle());
 
-            Assert.assertTrue(preferenceGroup.getPreferenceCount() == 2);
+			Assert.assertTrue(preferenceGroup.getPreferenceCount() == 2);
 
-            Preference firstSubPreference = preferenceGroup.getPreference(0);
-            Assert.assertEquals("Set language", firstSubPreference.getTitle());
-            Assert.assertEquals("Set the locale to use", firstSubPreference.getSummary());
+			Preference firstSubPreference = preferenceGroup.getPreference(0);
+			Assert.assertEquals("Set language", firstSubPreference.getTitle());
+			Assert.assertEquals("Set the locale to use", firstSubPreference.getSummary());
 
-            Preference secondSubPreference = preferenceGroup.getPreference(1);
-            Assert.assertEquals("Enable Goat Mode", secondSubPreference.getTitle());
-            Assert.assertEquals("Turns on Goat Mode for use with Goat Simulator", secondSubPreference.getSummary());
-        }
-    }
+			Preference secondSubPreference = preferenceGroup.getPreference(1);
+			Assert.assertEquals("Enable Goat Mode", secondSubPreference.getTitle());
+			Assert.assertEquals("Turns on Goat Mode for use with Goat Simulator", secondSubPreference.getSummary());
+		}
+	}
 }
