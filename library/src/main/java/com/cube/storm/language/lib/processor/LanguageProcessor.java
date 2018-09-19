87428bd2f5b7f1a73648ef63cee70dcba5341154
Tim Matthews <tim@3sidedcube.com>
2018-09-19 10:53:32 +0100

    Added test for newline processing

--- library/src/main/java/com/cube/storm/language/lib/processor/LanguageProcessor.java
+++ library/src/main/java/com/cube/storm/language/lib/processor/LanguageProcessor.java
@@ -1,10 +1,8 @@
 package com.cube.storm.language.lib.processor;
 
 import android.support.annotation.Nullable;
-
 import com.cube.storm.language.data.Language;
 import com.cube.storm.util.lib.processor.GsonProcessor;
-import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
@@ -12,7 +10,6 @@ import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -30,20 +27,18 @@ public class LanguageProcessor extends GsonProcessor<Language>
 
 		if (jsonElement != JsonNull.INSTANCE && jsonElement != null && jsonElement.isJsonObject())
 		{
-			Map<String, String> decoded = new Gson().fromJson(jsonElement, new TypeToken<Map<String, String>>(){}.getType());
+			Map<String, String> decoded = jsonDeserializationContext.deserialize(jsonElement, new TypeToken<Map<String, String>>(){}.getType());
 
 			//Remove double backslashes e.g., \\n to make display a new line
-			Iterator it = decoded.entrySet().iterator();
-			while (it.hasNext())
+			for (Map.Entry<String, String> pair : decoded.entrySet())
 			{
-				Map.Entry<String, String> pair = (Map.Entry<String,String>)it.next();
 				String temp = pair.getValue();
-				if(temp.contains("\\n")){
-					pair.setValue(temp.replace("\\n","\n"));
+				if (temp.contains("\\n"))
+				{
+					pair.setValue(temp.replace("\\n", "\n"));
 				}
 			}
 			language.setValues(decoded);
-
 			return language;
 		}
 
